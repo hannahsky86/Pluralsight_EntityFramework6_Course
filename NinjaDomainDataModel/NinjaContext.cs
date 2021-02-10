26f1217480fb61e545c88fcd794052e599de7c3e
Hannah Roach <hannahsky86@gmail.com>
2021-02-09 21:38:52 -0700

    Added DataModel

---
+++ NinjaDomainDataModel/NinjaContext.cs
@@ -0,0 +1,13 @@
+﻿using NinjaDomainClasses;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace NinjaDomain.Classes
+{
+    public class NinjaContext:DbContext
+    {
+        public DbSet<Ninja> Ninjas { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<NinjaEquipment> Equipment { get; set; }
+    }
+}
